1eefd9cbf48ca1e9df6d1c3ae6ca79446ab5c9d6
maxim-gh <max.verzilov32@gmail.com>
2021-03-23 19:40:34 +0500

    2

---
+++ 2/2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+
+namespace _2
+{
+    public class DayWeekEnum : IEnumerator
+    {
+        string[] arr;
+
+        int position = -1;
+        public object Current
+        {
+            get
+            {
+                return arr[position];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (position < arr.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+
+    public class DayWeek : IEnumerable
+    {
+        string[] days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        public IEnumerator GetEnumerator()
+        {
+            return days.GetEnumerator();
+        }
+
+    }
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            DayWeek dayweek = new DayWeek();
+            foreach (string days in dayweek)
+            {
+                Console.WriteLine($"День недели: {days}");
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
