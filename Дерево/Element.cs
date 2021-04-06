be87d716ab2a51b1b80c62595939b840dfe2a683
maxim-gh <max.verzilov32@gmail.com>
2021-04-06 20:04:13 +0500

    Дерево

---
+++ Дерево/Element.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Element
+{
+	public Element Right { get; set; }
+    public Element Left { get; set; }
+    public int Counter { get; set; }
+    public int Value { get; set; }
+}
--- Очередь и стек/Element.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-public class Element
-{
-    public Element Previous { get; set; }
-    public int Value { get; set; }
-}
